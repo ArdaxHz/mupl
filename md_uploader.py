c35ba38790d133cb5f29a2e0c60891bffbeb1189
Rudoal <70710586+Rudoal@users.noreply.github.com>
2022-01-12 19:57:09 +0000

    fix login

--- md_uploader.py
+++ md_uploader.py
@@ -14,7 +14,7 @@ from uuid import UUID
 import requests
 from natsort import natsorted
 
-__version__ = '0.6.1'
+__version__ = '0.7.0'
 
 languages = [{"english":"English","md":"en","iso":"eng"},{"english":"Japanese","md":"ja","iso":"jpn"},{"english":"Japanese (Romaji)","md":"ja-ro","iso":"jpn"},{"english":"Polish","md":"pl","iso":"pol"},{"english":"Serbo-Croatian","md":"sh","iso":"hrv"},{"english":"Dutch","md":"nl","iso":"dut"},{"english":"Italian","md":"it","iso":"ita"},{"english":"Russian","md":"ru","iso":"rus"},{"english":"German","md":"de","iso":"ger"},{"english":"Hungarian","md":"hu","iso":"hun"},{"english":"French","md":"fr","iso":"fre"},{"english":"Finnish","md":"fi","iso":"fin"},{"english":"Vietnamese","md":"vi","iso":"vie"},{"english":"Greek","md":"el","iso":"gre"},{"english":"Bulgarian","md":"bg","iso":"bul"},{"english":"Spanish (Es)","md":"es","iso":"spa"},{"english":"Portuguese (Br)","md":"pt-br","iso":"por"},{"english":"Portuguese (Pt)","md":"pt","iso":"por"},{"english":"Swedish","md":"sv","iso":"swe"},{"english":"Arabic","md":"ar","iso":"ara"},{"english":"Danish","md":"da","iso":"dan"},{"english":"Chinese (Simp)","md":"zh","iso":"chi"},{"english":"Chinese (Romaji)","md":"zh-ro","iso":"chi"},{"english":"Bengali","md":"bn","iso":"ben"},{"english":"Romanian","md":"ro","iso":"rum"},{"english":"Czech","md":"cs","iso":"cze"},{"english":"Mongolian","md":"mn","iso":"mon"},{"english":"Turkish","md":"tr","iso":"tur"},{"english":"Indonesian","md":"id","iso":"ind"},{"english":"Korean","md":"ko","iso":"kor"},{"english":"Korean (Romaji)","md":"ko-ro","iso":"kor"},{"english":"Spanish (LATAM)","md":"es-la","iso":"spa"},{"english":"Persian","md":"fa","iso":"per"},{"english":"Malay","md":"ms","iso":"may"},{"english":"Thai","md":"th","iso":"tha"},{"english":"Catalan","md":"ca","iso":"cat"},{"english":"Filipino","md":"tl","iso":"fil"},{"english":"Chinese (Trad)","md":"zh-hk","iso":"chi"},{"english":"Ukrainian","md":"uk","iso":"ukr"},{"english":"Burmese","md":"my","iso":"bur"},{"english":"Lithuanian","md":"lt","iso":"lit"},{"english":"Hebrew","md":"he","iso":"heb"},{"english":"Hindi","md":"hi","iso":"hin"},{"english":"Norwegian","md":"no","iso":"nor"},{"english":"Other","md":"NULL","iso":"NULL"}]
 http_error_codes = {
@@ -193,6 +193,7 @@ class AuthMD:
                  config: configparser.RawConfigParser):
         self.session = session
         self.config = config
+        self.first_login = True
         self.successful_login = False
         self.refresh_token = None
         self.token_file = root_path.joinpath(config["Paths"]["mdauth_path"])
@@ -204,7 +205,6 @@ class AuthMD:
                 token = json.load(login_file)
 
             refresh_token = token["refresh"]
-            self.refresh_token = refresh_token
             return refresh_token
         except (FileNotFoundError, json.JSONDecodeError):
             logging.error(
@@ -295,21 +295,28 @@ class AuthMD:
 
     def login(self, check_login = True):
         """Login to MD account using details or saved token."""
+
+        if not check_login and self.successful_login:
+            logging.info('Already logged in, not checking for login.')
+            return
+
         logging.info('Trying to login through the .mdauth file.')
 
-        if check_login:
-            logged_in = self._check_login()
-        else:
+        if self.first_login:
             self.refresh_token = self._open_auth_file()
             if self.refresh_token is None:
                 logged_in = self._login_using_details()
             else:
                 logged_in = self._refresh_token()
+        else:
+            logged_in = self._check_login()
 
         if logged_in:
             self.successful_login = True
-            logging.info(f'Logged into mangadex.')
-            print('Logged in.')
+            if self.first_login:
+                logging.info(f'Logged into mangadex.')
+                print('Logged in.')
+                self.first_login = False
         else:
             logging.critical("Couldn't login.")
             raise Exception("Couldn't login.")
@@ -829,8 +836,7 @@ class ChapterUploaderProcess:
         if not processed_zip:
             return
 
-        if 'Authorization' not in self.session.headers:
-            self.md_auth_object.login(False)
+        self.md_auth_object.login(False)
 
         upload_session_response_json = self._create_upload_session()
         if upload_session_response_json is None:
