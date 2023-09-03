7ba513e09af3fa4db069daec43e558e79320546c
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2023-09-03 19:01:01 +0600

    Complete refactor

--- uploader/utils/converter.py
+++ uploader/utils/converter.py
@@ -3,17 +3,18 @@ import io
 from PIL import Image, ImageSequence
 
 
-def get_new_format_for_webp(image: "Image") -> "str":
-    # If it has more then 1 frame it's animated so convert to GIF
-    try:
-        _ = ImageSequence.Iterator(image)[1]
-        return "GIF"
-    except IndexError:
-        pass
+def get_new_format_for_webp(image_bytes: "bytes") -> "str":
+    with Image.open(io.BytesIO(image_bytes)) as image:
+        # If it has more then 1 frame it's animated so convert to GIF
+        try:
+            _ = ImageSequence.Iterator(image)[1]
+            return "GIF"
+        except IndexError:
+            pass
 
-    # Lossless WebP and lossy (with alpha) WebP
-    if image.mode == "RGBA":
-        return "PNG"
+        # Lossless WebP and lossy (with alpha) WebP
+        if image.mode == "RGBA":
+            return "PNG"
 
-    # Otherwise, convert to JPEG
-    return "JPEG"
+        # Otherwise, convert to JPEG
+        return "JPEG"
