58343ea5341b16b4929997b88dadb35faecb6e9a
ArdaxHz <70710586+ArdaxHz@users.noreply.github.com>
2023-11-23 18:03:11 +0000

    add tag name to updater message

--- uploader/updater.py
+++ uploader/updater.py
@@ -31,15 +31,19 @@ def check_for_update():
         remote_version = version.parse(remote_release_json["tag_name"])
 
         if remote_version > local_version:
-            print(f"Update found: {local_version} => {remote_version}.")
-            logger.info(f"Update found: {local_version} => {remote_version}.")
+            print(
+                f"Update found: {local_version} => {remote_version}: {remote_release_json['name']}."
+            )
+            logger.info(
+                f"Update found: {local_version} => {remote_version}: {remote_release_json['name']}."
+            )
             if remote_version.major != local_version.major:
                 print(
                     f"""The new version may have breaking changes, please check the github releases page for a list of changes
                     https://github.com/ArdaxHz/mangadex_bulk_uploader/releases/latest"""
                 )
 
-            timeout = 5
+            timeout = 10
             t = Timer(timeout, raise_error, [ValueError("Not updating.")])
             t.start()
             answer = input("Do you want to update? [y/N] ")
@@ -72,7 +76,9 @@ def check_for_update():
                         fopen.write(file_data)
 
                 print(f"Updated, restart the uploader to run the new version.")
-                logger.info(f"Updated to version {remote_version}")
+                logger.info(
+                    f"Updated to version {remote_version}: {remote_release_json['name']}."
+                )
                 sys.exit()
         else:
             return False
